5c3ef0aca4a70d163465ad56cb44689d14c7fabc
Lucas <lucasgunner3@gmail.com>
2021-10-13 22:27:14 -0300

    Trabalhando com rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
+import { ReplacePipe } from './pipes/replace.pipe';
 import { StarComponent } from './star/star.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavbarComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: NotfoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -3,7 +3,6 @@ import { Course } from '../course/course';
 import { CourseService } from '../course/course.service';
 
 @Component({
-  selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.css']
 })
